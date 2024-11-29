462e9bc3ffa14f1f48d08e221300197d5f3555f9
ghaith <ghaithbouabda@gmail.com>
2024-11-29 18:10:04 +0100

    2nd commint

---
+++ todolistbackend/src/main/java/com/ghaith/todolistbackend/Task/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.ghaith.todolistbackend.Task;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
---
+++ todolistbackend/src/main/java/com/ghaith/todolistbackend/Task/TaskService.java
@@ -0,0 +1,45 @@
+package com.ghaith.todolistbackend.Task;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor  // Lombok annotation to generate a constructor with required arguments
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, Task taskDetails) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            task.setTitle(taskDetails.getTitle());
+            task.setCompleted(taskDetails.isCompleted());
+            return taskRepository.save(task);
+        }
+        return null;
+    }
+
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
